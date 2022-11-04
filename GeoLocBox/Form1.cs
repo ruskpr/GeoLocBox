e8e9e1f531e8d76b776e701550641f83e4d313fa
Rustu Koprulu <rustulp@gmail.com>
2022-11-04 14:09:01 -0600

    Add project files.

---
+++ GeoLocBox/Form1.cs
@@ -0,0 +1,61 @@
+using Phidget22;
+namespace GeoLocBox
+{
+    public partial class Form1 : Form
+    {
+        DigitalInput btnGreen = null;
+        DigitalInput btnRed = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+
+            // green
+            btnGreen = new DigitalInput();
+            btnGreen.Channel = 0;
+            btnGreen.HubPort = 0;
+            btnGreen.IsHubPortDevice = true;
+            //button1.StateChange += Button1_StateChange;
+            btnGreen.StateChange += Button1_StateChange;
+
+            btnGreen.Open(1000);
+
+            // red
+            btnRed = new DigitalInput();
+            btnRed.Channel = 0;
+            btnRed.HubPort = 1;
+            btnRed.IsHubPortDevice = true;
+            btnRed.StateChange += BtnRed_StateChange;
+            btnRed.Open(1000);
+        }
+
+        private void BtnRed_StateChange(object sender, Phidget22.Events.DigitalInputStateChangeEventArgs e)
+        {
+            if (this.BackColor == Color.White)
+            {
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+
+            SQLiteDataLayer dl = new SQLiteDataLayer("Data source =Z:/sqlite/myDB.db");
+
+            dl.InsertRecord("hello", DateTime.Now);
+        }
+
+        private void Button1_StateChange(object sender, Phidget22.Events.DigitalInputStateChangeEventArgs e)
+        {
+            //if (this.BackColor == Color.White)
+            //{
+            //    this.BackColor = Color.Green;
+            //}
+            //else
+            //{
+            //    this.BackColor = Color.White;
+            //}
+        }
+    }
+}
---
+++ GeoLocBox/SQLiteDataLayer.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLocBox
+{
+    public class SQLiteDataLayer
+    {
+        SqliteConnection conn;
+        SqliteCommand cmd;
+        string connectionString;
+
+        public SQLiteDataLayer(string connectionstring)
+        {
+            this.connectionString = connectionstring;
+        }
+
+        public void InsertRecord(string test_data, DateTime record_date)
+        {
+            using (SqliteConnection conn = new SqliteConnection(connectionString))
+            {
+                cmd = new SqliteCommand($"insert into TestTable (TestName) " +
+                    $"values ('{test_data}'", conn);
+                conn.Open();
+                // execute for no results, create, update, delete
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void DeleteRecord(int id)
+        {
+            //using (SqliteConnection conn = new SqliteConnection(connectionString))
+            //{
+            //    conn.Open();
+            //    cmd = new SqliteCommand($"delete from Table1 where id = '{id}'", conn);
+            //    cmd.ExecuteNonQuery();
+            //}
+
+        }
+        //public DataTable GetRecords()
+        //{
+        //    DataTable dt = new DataTable();
+        //    dt.Columns.Add("ID");
+        //    dt.Columns.Add("test_data");
+        //    dt.Columns.Add("test_date");
+        //    using (SqliteConnection conn = new SqliteConnection(connectionString))
+        //    {
+        //        conn.Open();
+        //        cmd = new SqliteCommand("select * from Table1", conn);
+
+        //        SqliteDataReader dr = cmd.ExecuteReader();
+        //        while (dr.Read())
+        //        {
+        //            dt.Rows.Add(dr[0], dr[1], dr[2]);
+        //        }
+        //    }
+
+        //    return dt;
+        //}
+        //public void UpdateRecord(int id, string update, DateTime record_date)
+        //{
+        //    using (SqliteConnection conn = new SqliteConnection(connectionString))
+        //    {
+        //        cmd = new SqliteCommand($"update Table1 set test_data = {update} where " +
+        //            $"ID = '{id}'", conn);
+        //        conn.Open();
+        //        // execute for no results, create, update, delete
+        //        cmd.ExecuteNonQuery();
+        //    }
+        //}
+    }
+
+}
